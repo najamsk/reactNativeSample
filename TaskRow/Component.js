780552cdb1dd0039b2c8549991d15662ec707f33
najam sikander awan <najamsk@gmail.com>
2016-11-30 12:22:06 +0500

    eslint added into project, animation for done task removed

--- TaskRow/Component.js
+++ TaskRow/Component.js
@@ -1,20 +1,16 @@
 import React from 'react';
-
 import {
-
   StyleSheet,
-
 } from 'react-native';
-
-import Render from './Render';
+import Render from './Render';  // eslint-disable-line import/no-unresolved
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     borderWidth: 1,
     borderColor: '#E7E7E7',
-    padding:20,
-    flex:1,
+    padding: 20,
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 20,
@@ -23,33 +19,30 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 20,
-    fontWeight:'300'
+    fontWeight: '300',
   },
-  doneButton:{
-    borderRadius:5,
+  doneButton: {
+    borderRadius: 5,
     backgroundColor: '#EAEAEA',
     padding: 5,
-  }
+  },
 });
 
-class TaskRow extends  React.Component {
-  constructor(props, context){
+class TaskRow extends React.Component {
+  constructor(props, context) {
     super(props, context);
     // this.state = {};
   }
 
-
-  onDonePressed(){
+  onDonePressed() {
     this.props.onDone(this.props.todo);
   }
 
-  render(){
+  render() {
     return Render.bind(this)(styles);
   }
 
-
 }
-
 
 TaskRow.propTypes = {
   onDone: React.PropTypes.func.isRequired,
